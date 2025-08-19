fb74b5b15e726223a88177b27af6b62bb1689786
SindhuMaccha <macchasindhu.888@gmail.com>
2025-08-19 17:44:52 +0530

    New class

---
+++ src/clearmethod/SindhuM.java
@@ -0,0 +1,21 @@
+package clearmethod;
+
+import org.testng.annotations.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.BeforeTest;
+
+public class SindhuM {
+	WebDriver driver;
+	  @Test
+	  public void f() {
+		  
+	  }
+	  @BeforeTest
+	  public void beforeTest() {
+		  driver = new ChromeDriver();
+		  driver.manage().window().maximize();
+	  }
+
+	}
+
